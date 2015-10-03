112ddf4b620a3e155501ff773b3e2b59ff9222d4
Surojit <surojit214@gmail.com>
2015-10-03 21:36:13 +0530

    test1

---
+++ src/com/app/rest/Rest.java
@@ -0,0 +1,10 @@
+package com.app.rest;
+
+/**
+ * Created by Surojit on 03-10-2015.
+ */
+public class Rest{
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
